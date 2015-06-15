44a7a59a8182425620636f6a68827f55c6a419c2
Adeel Shahid <imadeelshahid@gmail.com>
2015-06-15 16:08:36 +0200

    connecting to mysql db

--- zipkin/zipkin-generate-false-data.py
+++ zipkin/zipkin-generate-false-data.py
@@ -1,7 +1,8 @@
 import time
 import random
 
-import MySQLdb
+import sys
+import MySQLdb as mdb
 
 def ts_microseconds():
     return int( time.time() * 1000000 )
@@ -11,12 +12,26 @@ def generate_id():
 
 # reference to database connector
 def get_db_connection():
-    return MySQLdb.connect('localhost', 'zipkin', 'kinect', 'zipkin')
+    return mdb.connect('localhost', 'zipkin', 'kinect', 'zipkin')
 
-db = get_db_connection()
+conn = get_db_connection()
 
-print db
+try:
+    cur = conn.cursor()
+    cur.execute('SELECT VERSION()')
 
+    ver = cur.fetchone()
+
+    print 'Database version: %s ' % ver
+
+except mdb.Error, e:
+    print 'Error %d: %s' % (e.args[0], e.args[1])
+    sys.exit(1)
+
+finally:
+
+    if conn:
+        conn.close()
 
 # print 'timestamp: ' + str(ts_microseconds())
 # print 'random id: ' + str(generate_id())
