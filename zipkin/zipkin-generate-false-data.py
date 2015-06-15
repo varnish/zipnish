650b34f7d7ddaab098e873a72c724a2b2e04e357
Adeel Shahid <imadeelshahid@gmail.com>
2015-06-15 14:22:52 +0200

    generate random id 64 bits

--- zipkin/zipkin-generate-false-data.py
+++ zipkin/zipkin-generate-false-data.py
@@ -1,5 +1,11 @@
 import time
+import random
 
 def ts_microseconds():
     return int( time.time() * 1000000 )
 
+def generate_id():
+    return random.getrandbits(64)
+
+print 'timestamp: ' + str(ts_microseconds())
+print 'random id: ' + str(generate_id())
