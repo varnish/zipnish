3720a9b861f746af6984c9f711c0b1a018de0ebb
Adeel Shahid <imadeelshahid@gmail.com>
2015-06-16 13:05:15 +0200

    add_span

--- zipkin/zipkin-generate-false-data.py
+++ zipkin/zipkin-generate-false-data.py
@@ -16,6 +16,7 @@ def get_db_connection():
 
 conn = get_db_connection()
 
+'''
 try:
     cur = conn.cursor()
     cur.execute('SELECT VERSION()')
@@ -32,6 +33,33 @@ finally:
 
     if conn:
         conn.close()
+'''
+
+def add_span(span_name, span_id = None, trace_id = None, parent_id = None):
+    span_id = generate_id() if span_id is None else span_id
+    parent_id = 'NULL' if parent_id is None else parent_id
+    trace_id = generate_id() if trace_id is None else trace_id
+    debug = 0
+    duration = 0
+    created_ts = ts_microseconds()
+
+    sql = 'INSERT INTO zipkin_spans \
+            (span_id, parent_id, trace_id, span_name, debug, duration, created_ts) \
+            VALUES (\
+            {span_id}, {parent_id}, {trace_id},\
+            "{span_name}",\
+            {debug},\
+            {duration},\
+            {created_ts}\
+            )'.format(span_id=span_id, parent_id=parent_id, \
+            trace_id=trace_id, span_name=span_name, debug=debug, \
+            duration=duration, created_ts=created_ts)
+
+    return (span_id, trace_id)
+
+(span_id, trace_id) = add_span('test_span')
+
+print 'span_id = ', span_id , ', trace_id = ', trace_id
 
 # print 'timestamp: ' + str(ts_microseconds())
 # print 'random id: ' + str(generate_id())
