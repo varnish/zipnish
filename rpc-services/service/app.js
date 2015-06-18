e82c718d163efbf4338b4f30c115f3b4da8ac992
Adeel Shahid <imadeelshahid@gmail.com>
2015-06-18 15:08:42 +0200

    fixed variable name overlap

--- rpc-services/service/app.js
+++ rpc-services/service/app.js
@@ -3,16 +3,15 @@ var app = express();
 
 var argv = require('minimist')(process.argv.slice(2));
 
-console.log(argv);
-
 app.get('/', function (req, res) {
   res.send(argv.service + ' service');
 });
 
 var server = app.listen(argv.port, argv.address, function() {
-  var address = server.address(),
-    address = address.address,
-    port = address.port;
 
-  console.log(argv.service + ' service listening at http://%s:%s', address, port);
+  var address = server.address().address,
+    port = server.address().port;
+
+  console.log(argv.service + ' service listening at http://%s:%d', address, port);
+
 });
