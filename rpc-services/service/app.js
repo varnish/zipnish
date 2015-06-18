ef638aa5aec500bfe0b5e03b9e777f837cda76a7
Adeel Shahid <imadeelshahid@gmail.com>
2015-06-18 15:01:39 +0200

    changing host back to address

--- rpc-services/service/app.js
+++ rpc-services/service/app.js
@@ -9,10 +9,10 @@ app.get('/', function (req, res) {
   res.send(argv.service + ' service');
 });
 
-var server = app.listen(argv.port, argv.host, function() {
+var server = app.listen(argv.port, argv.address, function() {
   var address = server.address(),
-    host = address.address,
+    address = address.address,
     port = address.port;
 
-  console.log(argv.service + ' service listening at http://%s:%s', host, port);
+  console.log(argv.service + ' service listening at http://%s:%s', address, port);
 });
