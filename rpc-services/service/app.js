9c399a5dd108ce61b46b136b03a730fcc07d6be2
Adeel Shahid <imadeelshahid@gmail.com>
2015-06-18 15:00:46 +0200

    change command line parameters from address to host

--- rpc-services/service/app.js
+++ rpc-services/service/app.js
@@ -3,16 +3,16 @@ var app = express();
 
 var argv = require('minimist')(process.argv.slice(2));
 
+console.log(argv);
+
 app.get('/', function (req, res) {
   res.send(argv.service + ' service');
 });
 
-var server = app.listen(argv.port, argv.address, function() {
+var server = app.listen(argv.port, argv.host, function() {
   var address = server.address(),
     host = address.address,
     port = address.port;
-
-
 
   console.log(argv.service + ' service listening at http://%s:%s', host, port);
 });
