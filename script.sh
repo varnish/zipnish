17be3f2d92cd942218fea8fba02fbee07b99aa55
Adeel Shahid <imadeelshahid@gmail.com>
2015-06-19 13:51:38 +0200

    fetch product / payment information / credit card

--- script.sh
+++ script.sh
@@ -11,3 +11,17 @@ fi
 # Process Order
 node app.js --port 5000 --address 127.0.0.1 --service 'Process Order'
 
+
+# Fetch Customer
+node app.js --port 5010 --address 127.0.0.1 --service 'Fetch Customer'
+
+# Fetch Product
+node app.js --port 5011 --address 127.0.0.1 --service 'Fetch Product'
+
+
+# Get Payment Information
+node app.js --port 5020 --address 127.0.0.1 --service 'Get Payment Information'
+
+# Validate Credit Card
+node app.js --port 5021 --address 127.0.0.1 --service 'Validate Credit Card'
+node app.js --port 5022 --address 127.0.0.1 --service 'Verify Credit Card'
