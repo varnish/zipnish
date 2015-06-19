911d61e33d6bdd24bd4c22a51100287b761611b3
Adeel Shahid <imadeelshahid@gmail.com>
2015-06-19 13:34:14 +0200

    only terminate services option added

--- script.sh
+++ script.sh
@@ -3,4 +3,10 @@
 # terminate any existing node web services
 kill $(ps au | grep node | grep -v 'grep' | awk '{print $2}')
 
+if [ $terminate ]
+then
+  echo 'just terminate'
+  exit
+fi
 
+echo 'after terminate'
