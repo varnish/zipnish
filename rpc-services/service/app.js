a705d1bed07e13ff81985416354e903b32bf0738
Adeel Shahid <imadeelshahid@gmail.com>
2015-06-19 10:51:27 +0200

    command line support for single / multiple rpc-calls

--- rpc-services/service/app.js
+++ rpc-services/service/app.js
@@ -1,10 +1,29 @@
+var http = require('http');
 var express = require('express');
 var app = express();
 
 var argv = require('minimist')(process.argv.slice(2));
 
+console.log('argv.call', argv.call);
+
 app.get('/', function (req, res) {
-  res.send(argv.service + ' service');
+
+  var rpcCalls = [];
+
+  if (argv.call) {
+    if (Array.isArray(argv.call)) {
+      rpcCalls = argv.call.slice();
+    } else if (argv.call && argv.call.length) {
+      rpcCalls.push( argv.call );
+    }
+  }
+
+  if (rpcCalls.length > 0) {
+    // single / multiple rpc calls
+  } else {
+    res.send(argv, service);
+  }
+
 });
 
 var server = app.listen(argv.port, argv.address, function() {
