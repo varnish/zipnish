19a2a43381570a99ea41d523dd0ca317c63681cc
Adeel Shahid <imadeelshahid@gmail.com>
2015-06-23 11:24:44 +0200

    modularizing parser code

--- rpc-service/app.js
+++ rpc-service/app.js
@@ -2,71 +2,21 @@ var http = require('http'),
   async = require('async'),
   express = require('express');
 
+var parser = require('./services/parser');
+
 var app = express(),
   argv = require('minimist')(process.argv.slice(2));
 
-function parseChildrenServices(childService)
-{
-  var callFlow, childServices, childrenURLs, temp;
+var services = parser.parseServices(argv.services);
 
-  temp = childService.split(':');
+console.log( services );
 
-  callFlow = temp[0];
-  childrenURLs = temp[1].split(',');
-
-  var data = {
-    flow: callFlow,
-    urls: childrenURLs
-  };
-
-  return data;
-}
-
-function parseService(argService)
-{
-  var serviceURLAndLabel, startIdx, endIdx, hasChildrenServices;
-
-
-  hasChildrenServices = false;
-  endIdx = argService.indexOf('=>');
-
-  if (endIdx > -1) {
-    hasChildrenServices = true;
-    serviceURLAndLabel = argService.substr(0, endIdx);
-  } else {
-    serviceURLAndLabel = argService;
-  }
-
-  var service = {}, temp;
-
-  temp = serviceURLAndLabel.split(':');
-  service.url = temp[0];
-  service.label = temp[1];
-
-  if (hasChildrenServices) {
-    service.children = parseChildrenServices( argService.substr(endIdx + 2) );
-  }
-
-  return service;
-}
-
-function parseServices(argServices)
-{
-  var services = [];
-
-  for (var i = 0; i < argServices.length; i++) {
-    services.push( parseService( argServices[i] ) );
-  }
-
-  return services;
-}
-
-var services = parseServices(argv.services);
 
 app.get('/*', function (req, res) {
 
-  res.send( req.url );
+  //var service = global.findService(req.url);
 
+  req.send( req.url );
 
 });
 
