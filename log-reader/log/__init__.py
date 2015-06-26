36c1d064803b0d64282b58310d82301d73874cb2
Adeel Shahid <imadeelshahid@gmail.com>
2015-06-26 10:58:46 +0200

    moving LogDataManager code to a module

---
+++ log-reader/log/__init__.py
@@ -0,0 +1,1 @@
+from LogDataManager import LogDataManager
--- log-reader/sample_varnishlog.py
+++ log-reader/sample_varnishlog.py
@@ -1,36 +1,5 @@
 import varnishapi,time,os,sys,syslog,traceback
-
-# LogDataManager responsible for recording of log data
-class LogDataManager:
-    def __init__(self):
-        self.logReq = {}
-        self.logBereq = {}
-
-    def addLogItem(self, vxid, requestType, tag, data):
-
-        #print "type: %s, vxid: %d, tag: %s, data: %s" % (requestType, vxid, tag, data)
-
-        if type == 'c' and tag == 'Begin':
-            self.logReq = {}
-            self.logBereq = {}
-        elif tag == 'End':
-            if requestType == 'b':
-                print self.logReq
-                print self.logBereq
-
-                self.logBereq = {}
-                self.logReq = {}
-
-        if vxid > 0:
-            if requestType == 'b':
-                self.logReq[tag] = data.rstrip('\x00')
-            elif requestType == 'c':
-                self.logBereq[tag] = data.rstrip('\x00')
-
-    # separate, may be we can do bulk sql inserts later on
-    def pushLogForVxId(self, vxid):
-        # sql query for insertion
-        return
+from log import LogDataManager
 
 # LogReader - read and do basic processing of incoming data
 class LogReader:
