7588224de442ff0734bb594b83b2f5bca18f60b8
Adeel Shahid <imadeelshahid@gmail.com>
2015-06-26 11:14:01 +0200

    log storage

---
+++ log-reader/log/LogStorage.py
@@ -0,0 +1,10 @@
+import time
+
+# LogStorage - read and do basic processing of incoming data
+class LogStorage:
+    def __init__(self):
+        self.logDataManager = logDataManager
+        self.table = [];
+
+    def push(self, row):
+        print row
