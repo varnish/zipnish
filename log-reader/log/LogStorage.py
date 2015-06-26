5410017a0dd451f187ab16c2b3154b0a70691f20
Adeel Shahid <imadeelshahid@gmail.com>
2015-06-26 11:37:34 +0200

    pprint to print output

--- log-reader/log/LogStorage.py
+++ log-reader/log/LogStorage.py
@@ -1,9 +1,11 @@
 import time
+import pprint
 
 # LogStorage - read and do basic processing of incoming data
 class LogStorage:
     def __init__(self):
         self.table = [];
+        self.pp = pprint.PrettyPrinter(indent=4)
 
     def push(self, obj):
-        print obj
+        self.pp.pprint(obj)
