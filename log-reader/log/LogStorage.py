0467e045a73858e960e89bc3ff23d19437a0b9e7
Adeel Shahid <imadeelshahid@gmail.com>
2015-06-26 11:53:22 +0200

    buffer minimum records before flushing them

--- log-reader/log/LogStorage.py
+++ log-reader/log/LogStorage.py
@@ -1,11 +1,19 @@
 import time
-import pprint
+from tabulate import tabulate
 
 # LogStorage - read and do basic processing of incoming data
 class LogStorage:
     def __init__(self):
-        self.table = [];
-        self.pp = pprint.PrettyPrinter(indent=4)
+        self.rows = []
+        self.minNumOfRecordsForFlush = 5
 
     def push(self, obj):
-        self.pp.pprint(obj)
+        self.rows.append([ obj['Timestamp'] ])
+
+        if len(self.rows) > self.minNumOfRecordsForFlush:
+            self.flush()
+
+    def flush(self):
+        print tabulate(self.rows)
+        self.rows = []
+
