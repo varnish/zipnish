64365f957f1d56e1593e7edb8628b8d6a2a490d6
Adeel Shahid <imadeelshahid@gmail.com>
2015-07-01 13:22:35 +0200

    truncateTables() before beginning, tested code

--- log-reader/log/LogDatabase.py
+++ log-reader/log/LogDatabase.py
@@ -10,20 +10,21 @@ class LogDatabase:
         self.tables = ['spans', 'annotations']
 
         # connect to database
-        self.conn = SimpleMysql(\
+        self.db = SimpleMysql(\
                     host=keyVals['host'], \
                     db=keyVals['db'], \
                     user=keyVals['user'], \
-                    passwd=keyVals['passwd'] \
+                    passwd=keyVals['passwd'], \
+                    keep_alive=keyVals['keep_alive'] \
                 )
 
-        if 'truncateTables' in keyVals:
+        if 'truncate_tables' in keyVals:
             self.truncateTables()
 
     def getParams(self):
         return self.dbParams
 
-    def getConnection(self):
+    def getDB(self):
         return self.conn
 
     def insert(self, table, **params):
@@ -31,6 +32,15 @@ class LogDatabase:
 
     # truncate data in tables related to our application
     def truncateTables(self):
-        if self.conn is not None:
+        print 'Truncating tables'
+
+        if self.db is not None and self.db.is_open():
             for tableName in self.tables:
-                self.conn.delete(self.tablesPrefix + tableName)
+                print 'truncating table: ' + table
+
+                # table prefix + table name
+                table = self.tablesPrefix + tableName
+
+                # delete table, and commit changes to database
+                self.db.delete(table)
+                self.db.commit()
