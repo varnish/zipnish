9e7ad10eb6115a39155c80f658e431af56e5cc52
Adeel Shahid <imadeelshahid@gmail.com>
2015-07-01 11:11:06 +0200

    connect to mysql database

--- log-reader/log/LogDatabase.py
+++ log-reader/log/LogDatabase.py
@@ -3,6 +3,15 @@ from simplemysql import SimpleMysql
 class LogDatabase:
     def __init__(self, **args):
         self.dbParams = args
+        self.conn = SimpleMysql(\
+                    host=args['host'], \
+                    db=args['db'], \
+                    user=args['user'], \
+                    passwd=args['passwd'] \
+                )
 
     def getParams(self):
         return self.dbParams
+
+    def getConnection(self):
+        return self.conn
