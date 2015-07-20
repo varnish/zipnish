1086ab85cbd8a38c3a6375db1958ff3f58fec759
Adeel Shahid <imadeelshahid@gmail.com>
2015-07-20 13:23:46 +0200

    load app configuration

--- ui/app/__init__.py
+++ ui/app/__init__.py
@@ -4,6 +4,6 @@ from config import config
 def create_app(config_name):
     app = Flask(__name__)
 
-    app.config_from_object(config[config_name])
+    app.config.from_object(config[config_name])
 
     return app
