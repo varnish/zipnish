0c8876df8424f3c4dfee144c0ec2187c6d6d4536
Adeel Shahid <imadeelshahid@gmail.com>
2015-07-20 13:52:34 +0200

    host, port, user, pass configuration for database

--- ui/config.py
+++ ui/config.py
@@ -6,6 +6,11 @@ class Config(object):
     DEBUG = False
     TESTING = False
 
+    DB_HOST = os.environ.get('DB_HOST')
+    DB_PORT = os.environ.get('DB_PORT')
+    DB_USER = os.environ.get('DB_USER')
+    DB_PASS = os.environ.get('DB_PASS')
+
     @staticmethod
     def init_app(app):
         pass
