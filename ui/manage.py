0823c5266caf97f05524b1ff3b1e7f99f95e1911
Adeel Shahid <imadeelshahid@gmail.com>
2015-07-20 13:42:23 +0200

    make shell context

--- ui/manage.py
+++ ui/manage.py
@@ -7,5 +7,9 @@ from flask.ext.script import Shell, Manager
 app = create_app(os.getenv('APP_CONFIG') or 'default')
 manager = Manager(app)
 
+def make_shell_context():
+    return dict(app=app)
+
+
 if __name__ == '__main__':
     manager.run()
