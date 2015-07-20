bfacf7a7ac4adee1b3c3052770f8424c1638778e
Adeel Shahid <imadeelshahid@gmail.com>
2015-07-20 13:43:26 +0200

    add shell command and initialize with shell context

--- ui/manage.py
+++ ui/manage.py
@@ -10,6 +10,7 @@ manager = Manager(app)
 def make_shell_context():
     return dict(app=app)
 
+manager.add_command('shell', Shell(make_context=make_shell_context))
 
 if __name__ == '__main__':
     manager.run()
