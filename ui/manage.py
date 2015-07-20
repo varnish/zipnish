1bf27a47e066c334491b7bd57decea9cb672d42b
Adeel Shahid <imadeelshahid@gmail.com>
2015-07-20 13:35:16 +0200

    pass app to manager

--- ui/manage.py
+++ ui/manage.py
@@ -2,10 +2,12 @@
 import os
 
 from app import create_app
+from flask.ext.script import Manager
 
 app = create_app(os.getenv('APP_CONFIG') or 'default')
+manager = Manager(app)
 
-print app
+print manager
 
 if __name__ == '__main__':
     app.run()
