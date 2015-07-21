959c7266f76db6fc40c184c3bb24d24599ce1033
Adeel Shahid <imadeelshahid@gmail.com>
2015-07-21 10:36:04 +0200

    comments and create sqlalchemy

--- ui/app/__init__.py
+++ ui/app/__init__.py
@@ -1,6 +1,16 @@
+# web framework
 from flask import Flask
+
+# extensions
+from flask.ext.sqlalchemy import SQLAlchemy
+
+# configuration
 from config import config
 
+# initialization
+db = SQLAlchemy()
+
+# called from the main app to create an application instance
 def create_app(config_name):
     app = Flask(__name__)
 
