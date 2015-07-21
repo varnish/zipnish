f355197288c13a407a63f0815c90e73541d9f59a
Adeel Shahid <imadeelshahid@gmail.com>
2015-07-21 10:42:19 +0200

    amending SQLALCHEMY_DATABASE_URI

--- ui/config.py
+++ ui/config.py
@@ -12,7 +12,7 @@ class Config(object):
     DB_PASS = os.environ.get('DB_PASS')
     DB_NAME = os.environ.get('DB_NAME')
 
-    SQLALCHEMY_DATABASE_URI = "mysql://{}:{}@{}:{}/{}".\
+    SQLALCHEMY_DATABASE_URI = "mysql+mysqldb://{}:{}@{}:{}/{}".\
             format(DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME)
 
     @staticmethod
