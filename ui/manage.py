f1450e2d2b426585f07735098df72cc926d5ccc6
Adeel Shahid <imadeelshahid@gmail.com>
2015-07-21 10:34:08 +0200

    add sqlalchemy to the mix

--- ui/manage.py
+++ ui/manage.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 import os
 
-from app import create_app
+from app import create_app, db
 from flask.ext.script import Shell, Manager
 
 app = create_app(os.getenv('APP_CONFIG') or 'default')
 manager = Manager(app)
 
 def make_shell_context():
-    return dict(app=app)
+    return dict(app=app, db=db)
 
 manager.add_command('shell', Shell(make_context=make_shell_context))
 
