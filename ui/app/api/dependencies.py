2bd13a7d035918650410374b03e09062007c123a
Adeel Shahid <imadeelshahid@gmail.com>
2015-07-24 11:30:05 +0200

    time bound and non-time bound dependencies

--- ui/app/api/dependencies.py
+++ ui/app/api/dependencies.py
@@ -1,6 +1,10 @@
 from flask import render_template, request, redirect
 from . import api
 
-@api.route('/dependencies')
-def query():
+@api.route('/dependencies', methods=['GET'])
+def dependencies():
     return '/dependencies'
+
+@api.route('/dependencies/<start_time>/<end_time>', methods=['GET'])
+def dependenciesTimeBound(start_time=None, end_time=None):
+    return '/dependencies/%s/%s' % (start_time, end_time)
