6113a4be031a04a3b89c50d410e0a3e1e28b5b36
Adeel Shahid <imadeelshahid@gmail.com>
2015-07-24 11:14:43 +0200

    is_pinned end-point

--- ui/app/api/pin.py
+++ ui/app/api/pin.py
@@ -1,6 +1,10 @@
 from flask import render_template, request, redirect
 from . import api
 
+@api.route('/pin/is_pinned/<pin_id>', methods=['GET'])
+def is_pinned(pin_id):
+    return '/api/is_pinned/%s' % pin_id
+
 @api.route('/pin/<pin_id>/<state>', methods=['GET'])
 def pin(pin_id, state):
     return '/api/pin/%s/%s' % (pin_id, state)
