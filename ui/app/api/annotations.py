4011023d29d06a3cb535486e3853d492347f159d
Adeel Shahid <imadeelshahid@gmail.com>
2015-07-24 11:24:17 +0200

    top key-value annotations

--- ui/app/api/annotations.py
+++ ui/app/api/annotations.py
@@ -4,3 +4,7 @@ from . import api
 @api.route('/top_annotations')
 def top_annotations():
     return '/top_annotations'
+
+@api.route('/top_kv_annotations')
+def top_kv_annotations():
+    return '/top_kv_annotations'
