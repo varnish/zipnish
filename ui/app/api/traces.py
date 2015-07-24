d48be5bfa6bf4d48243ad99b9058eb9edc03b52a
Adeel Shahid <imadeelshahid@gmail.com>
2015-07-24 11:35:43 +0200

    traces end points in api, /get, /trace

--- ui/app/api/traces.py
+++ ui/app/api/traces.py
@@ -1,6 +1,7 @@
 from flask import render_template, request, redirect
 from . import api
 
+@api.route('/get/<trace_id>')
 @api.route('/trace/<trace_id>')
 def trace(trace_id):
     return '/trace/%s' % trace_id
