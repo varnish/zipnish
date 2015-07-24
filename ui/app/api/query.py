f10c94067439e0032cce8122053783a4a6ca37e2
Adeel Shahid <imadeelshahid@gmail.com>
2015-07-24 11:18:57 +0200

    end point url fix

--- ui/app/api/query.py
+++ ui/app/api/query.py
@@ -1,6 +1,6 @@
 from flask import render_template, request, redirect
 from . import api
 
-@api.route('/pin/query')
-def query(pin_id):
-    return '/pin/query'
+@api.route('/query')
+def query():
+    return '/query'
