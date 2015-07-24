91ef20565e5c1ec5bd26277aa429484271bb2581
Adeel Shahid <imadeelshahid@gmail.com>
2015-07-24 10:59:26 +0200

    only allow GET on trace

--- ui/app/traces/views.py
+++ ui/app/traces/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, request, redirect
 from . import traces
 
-@traces.route('/<trace_id>')
+@traces.route('/<trace_id>', methods=['GET'])
 def traces(trace_id):
     return '/trace/%s' % trace_id
