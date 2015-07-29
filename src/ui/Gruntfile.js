ac7d43fb9cfe6894e14810fba06363f198d0499d
Muhammad Adeel <adeel@varnish-software.com>
2015-07-29 13:29:02 +0200

    restrict default watch task to only css files

--- src/ui/Gruntfile.js
+++ src/ui/Gruntfile.js
@@ -61,5 +61,5 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
 	// Default Task
-	grunt.registerTask('default', ['watch']);
+	grunt.registerTask('default', ['watch:css']);
 };
