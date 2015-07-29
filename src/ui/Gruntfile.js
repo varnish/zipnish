834c5d29a38acd744e1de7555dd9c1c35207f134
Muhammad Adeel <adeel@varnish-software.com>
2015-07-29 13:06:08 +0200

    add default task to grunt, and refer it to watch

--- src/ui/Gruntfile.js
+++ src/ui/Gruntfile.js
@@ -41,4 +41,6 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
+	// Default Task
+	grunt.registerTask('default', ['watch']);
 };
