b3f100c4d251113871cac3bf2c294f4f9f9c15e9
Muhammad Adeel <adeel@varnish-software.com>
2015-07-29 12:51:03 +0200

    clean existing bootstrap file

--- src/ui/Gruntfile.js
+++ src/ui/Gruntfile.js
@@ -1,7 +1,11 @@
 module.exports = function (grunt) {
 
 	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json')
+		pkg: grunt.file.readJSON('package.json'),
+
+		clean: {
+			bootstrap: ['ui/app/static/css/bootstrap.css']
+		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-clean');
