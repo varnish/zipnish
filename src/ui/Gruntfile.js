786768e4fada05537018d3d52ca7ccad35170a52
Muhammad Adeel <adeel@varnish-software.com>
2015-07-29 12:49:35 +0200

    initialize configuration for Gruntfile

--- src/ui/Gruntfile.js
+++ src/ui/Gruntfile.js
@@ -1,5 +1,9 @@
 module.exports = function (grunt) {
 
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json')
+	});
+
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
