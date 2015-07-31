9fe92a7a6a0f5ad390166d2379942874db232275
Adeel Shahid <imadeelshahid@gmail.com>
2015-07-31 13:45:59 +0200

    amending Spans model to have primary_key, to pass over sqlAlchemy limitations

--- ui/app/models.py
+++ ui/app/models.py
@@ -2,13 +2,13 @@ from . import db
 
 class Spans(db.Model):
     __tablename__ = 'zipkin_spans'
-    span_id = db.Column(db.Integer)
+    span_id = db.Column(db.Integer, primary_key=True)
     parent_id = db.Column(db.Integer)
     trace_id = db.Column(db.Integer)
     span_name = db.Column(db.String(255))
     debug = db.Column(db.Integer)
     duration = db.Column(db.Integer)
-    created_ts = db.Column(db.Integer)
+    created_ts = db.Column(db.Integer, primary_key=True)
 
     def __repr__(self):
         return '<Span %r>' % self.span_name
