f61221ed525d78ba407669c4bd2a83d603b2cdb7
Adeel Shahid <imadeelshahid@gmail.com>
2015-07-31 12:52:35 +0200

    fields for Annotations

--- ui/app/models.py
+++ ui/app/models.py
@@ -12,3 +12,12 @@ class Spans(db.Model):
 
 class Annotations(db.Model):
     __tablename__ = 'zipkin_annotations'
+    span_id = db.Column(db.Integer)
+    trace_id = db.Column(db.Integer)
+    span_name = db.Column(db.String(255))
+    service_name = db.Column(db.String(255))
+    value = db.Column(db.Text)
+    ipv4 = db.Column(db.Integer)
+    port = db.Column(db.Integer)
+    a_timestamp = db.Column(db.Integer)
+    duration = db.Column(db.Integer)
