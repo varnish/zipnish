57e95141c3cc1acc83fc92156584bc1381c27a7b
Adeel Shahid <imadeelshahid@gmail.com>
2015-07-31 15:34:52 +0200

    show span names inside a dropdown

--- ui/app/index/views.py
+++ ui/app/index/views.py
@@ -1,6 +1,17 @@
 from flask import request, redirect, render_template
 from . import index
 
+from .. import db
+
 @index.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    connection = db.engine.connect()
+    result = connection.execute("SELECT DISTINCT span_name FROM zipkin_spans")
+
+    spans = []
+    for row in result:
+        spans.append( row['span_name'] )
+
+    connection.close()
+
+    return render_template('index.html', spans=spans)
