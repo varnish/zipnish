ff3ca6636231bb1a4f62a5bf35821f2d59c4dfc1
Adeel Shahid <imadeelshahid@gmail.com>
2015-07-31 12:42:30 +0200

    fields for model Spans

--- ui/app/models.py
+++ ui/app/models.py
@@ -2,3 +2,10 @@ from . import db
 
 class Spans(db.Model):
     __tablename__ = 'zipkin_spans'
+    span_id = db.Column(db.Integer)
+    parent_id = db.Column(db.Integer)
+    trace_id = db.Column(db.Integer)
+    span_name = db.Column(db.String(255))
+    debug = db.Column(db.Integer)
+    duration = db.Column(db.Integer)
+    created_ts = db.Column(db.Integer)
