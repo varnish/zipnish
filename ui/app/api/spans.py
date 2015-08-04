d39133226c8c92b92613765736ba9771225c8143
Adeel Shahid <imadeelshahid@gmail.com>
2015-08-04 13:06:34 +0200

    filling in /api/spans

--- ui/app/api/spans.py
+++ ui/app/api/spans.py
@@ -1,6 +1,27 @@
-from flask import render_template, request, redirect
-from . import api
+import json
 
-@api.route('/spans')
+from flask import render_template, request, redirect, make_response
+
+from . import api
+from .. import db
+
+@api.route('/spans', methods=['GET'])
 def spans():
-    return '/spans'
+    # read GET values
+    serviceName = request.args.get('serviceName')
+
+    # get database engine connection
+    connection = db.engine.connect()
+
+    # query for results
+    spans = []
+    spansQuery = "SELECT DISTINCT span_name \
+            FROM zipkin_annotations \
+            WHERE service_name='%s'" % serviceName
+
+    result = connection.execute(spansQuery)
+
+    for row in result:
+        spans.append( row['span_name'] )
+
+    return json.dumps(spans)
