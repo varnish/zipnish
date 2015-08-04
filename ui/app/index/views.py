9cc500520d2320e5e86b1b8683c5f973db3a05c5
Adeel Shahid <imadeelshahid@gmail.com>
2015-08-04 11:01:12 +0200

    pass service and span values to view

--- ui/app/index/views.py
+++ ui/app/index/views.py
@@ -29,4 +29,4 @@ def index():
     # close connection
     connection.close()
 
-    return render_template('index.html', spans=spans, services=services, spanName=spanName, serviceName=serviceName)
+    return render_template('index.html', spans=spans, services=services, getSpanName=spanName, getServiceName=serviceName)
