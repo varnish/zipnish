8d3acb19387acf5283b229063a3032e4b624e8aa
Adeel Shahid <imadeelshahid@gmail.com>
2015-08-10 13:12:15 +0200

    convert long to trace compatible hex string

--- ui/app/utils.py
+++ ui/app/utils.py
@@ -1,2 +1,13 @@
 def GenerateTraceURLId(idLong):
-    pass
+    hexStr = ''
+
+    hexStr += format(idLong >> 56 & 0xff, 'x')
+    hexStr += format(idLong >> 48 & 0xff, 'x')
+    hexStr += format(idLong >> 40 & 0xff, 'x')
+    hexStr += format(idLong >> 32 & 0xff, 'x')
+    hexStr += format(idLong >> 24 & 0xff, 'x')
+    hexStr += format(idLong >> 16 & 0xff, 'x')
+    hexStr += format(idLong >>  8 & 0xff, 'x')
+    hexStr += format(idLong       & 0xff, 'x')
+
+    return hexStr
