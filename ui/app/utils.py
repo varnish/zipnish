9829990619f00857cd2f37bd78e30d9b6ebb4023
Adeel Shahid <imadeelshahid@gmail.com>
2015-08-11 10:46:41 +0200

    simplify hex / long conversions

--- ui/app/utils.py
+++ ui/app/utils.py
@@ -1,18 +1,7 @@
 import sys
 
 def GenerateTraceURLId(idLong):
-    hexStr = ''
-
-    hexStr += format(idLong >> 56 & 0xff, 'x')
-    hexStr += format(idLong >> 48 & 0xff, 'x')
-    hexStr += format(idLong >> 40 & 0xff, 'x')
-    hexStr += format(idLong >> 32 & 0xff, 'x')
-    hexStr += format(idLong >> 24 & 0xff, 'x')
-    hexStr += format(idLong >> 16 & 0xff, 'x')
-    hexStr += format(idLong >>  8 & 0xff, 'x')
-    hexStr += format(idLong       & 0xff, 'x')
-
-    return hexStr
+    return hex(idLong)
 
 def ParseTraceURLId(hexString):
-    return int(hexString, 16)
+    return long(hexString, 16)
