a8b9d8ec6bedfb6244bdf75e42e404be02525946
Adeel Shahid <imadeelshahid@gmail.com>
2015-08-11 11:00:55 +0200

    query for span information

--- ui/app/traces/views.py
+++ ui/app/traces/views.py
@@ -1,8 +1,26 @@
 from flask import render_template, request, redirect
+
 from . import traces
+from .. import db
+
 from ..utils import ParseTraceURLId
 
-@traces.route('/<trace_id>', methods=['GET'])
-def traces(trace_id):
-    return str(ParseTraceURLId(trace_id))
+
+@traces.route('/<hex_trace_id>', methods=['GET'])
+def traces(hex_trace_id):
+    # hex trace_id converted to long
+    traceId = str(ParseTraceURLId(hex_trace_id))
+
+    # find the number of DISTINCT spans, that above service connects with
+    query = "SELECT COUNT(DISTINCT span_id) as spanCount, parent_id, created_ts, trace_id \
+            FROM zipkin_spans \
+            GROUP BY trace_id \
+            HAVING \
+            trace_id IN (%s) \
+            ORDER BY created_ts DESC" \
+            % str(traceId)
+    result = connection.execute(query)
+
+
+
     return render_template('trace.html')
