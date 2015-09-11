0bc4535aa5b17cebfdf8243b165c678538a10fe7
Adeel Shahid <imadeelshahid@gmail.com>
2015-09-11 11:50:18 +0200

    remove table truncating, when connecting to database

--- log-reader/app.py
+++ log-reader/app.py
@@ -35,7 +35,7 @@ if __name__ == '__main__':
                     'user':     'zipkin', \
                     'passwd':   'kinect', \
                     'keep_alive': True, \
-                    'truncate_tables': True \
+                    'truncate_tables': False \
                 }
 
     # connect to log database
