0fcda0909c6870677ee6b975da818d0d1c9b9727
Muhammad Adeel <adeel@varnish-software.com>
2015-10-15 16:12:09 +0200

    change ip for rpc service

--- script.sh
+++ script.sh
@@ -17,7 +17,7 @@ fi
 # Change directory to rpc-service/
 cd rpc-service
 
-node app.js --port 9000 --proxy-port 6081 --address 127.0.0.1 --service 'Process Order' \
+node app.js --port 9000 --proxy-port 6081 --address 192.168.33.14 --service 'Process Order' \
             --services '/process-order:Process Order=>serial:/fetch-customer,/fetch-products,/get-payment-information' \
             --services '/fetch-customer:Fetch Customer' \
             --services '/fetch-products:Fetch Products=>serial:/fetch-product' \
