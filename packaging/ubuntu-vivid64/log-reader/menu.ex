27ba0f664b82a2b5be178c570cdf2fec90c45a66
Muhammad Adeel <adeel@varnish-software.com>
2015-11-23 14:05:15 +0100

    log-reader specific configuration changes

--- packaging/ubuntu-vivid64/log-reader/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(zipnish-ui):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="zipnish-ui" command="/usr/bin/zipnish-ui"
--- packaging/ubuntu-vivid64/log-reader/zipnish-ui.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the zipnish-ui package
-#
-0 4	* * *	root	[ -x /usr/bin/zipnish-ui_maintenance ] && /usr/bin/zipnish-ui_maintenance
