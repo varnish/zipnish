9eb78bc3c494fb560f05d2e9add078599610a692
Marius Magureanu <mariusmagureanu@live.dk>
2016-04-07 14:36:50 +0200

    A snapshot vsl binding and its parser have been added.

---
+++ logreader/log/log_snapshot.py
@@ -0,0 +1,64 @@
+URL_TAGS = ['ReqURL', 'BereqURL']
+HEADER_TAGS = ['ReqHeader', 'RespHeader', 'BereqHeader', 'BerespHeader']
+
+
+class Snapshot(object):
+
+    def __init__(self):
+        self.log_snapshot = dict()
+        self.callbacks = []
+
+    def add_callback_func(self, func):
+        self.callbacks.append(func)
+
+    def fill_snapshot(self, vxid, request_type, tag, data):
+        if tag == 'Begin':
+            self.log_snapshot.clear()
+            self.log_snapshot['begin'] = data
+        elif tag == 'End':
+            self.log_snapshot['request_type'] = request_type
+            self.__on_snapshot_ready()
+            self.log_snapshot.clear()
+
+        if tag in URL_TAGS:
+            self.log_snapshot['span_name'] = data.rstrip('\x00')
+        elif tag == 'Link':
+            value = data.split(" ")[1]
+            self.log_snapshot['link'] = value
+        elif tag == 'Timestamp':
+            ts = data.rstrip('\x00').split(" ")
+            assert len(ts) == 4
+
+            ts_name_tag = ts[0].rstrip(":")
+            ts_abs_tag = "timestamp-%s-%s" % ("abs", ts_name_tag)
+            ts_duration_tag = "timestamp-%s-%s" % ("duration", ts_name_tag)
+            self.log_snapshot[ts_abs_tag] = ts[1]
+            self.log_snapshot[ts_duration_tag] = ts[3]
+        elif tag in HEADER_TAGS:
+            header_info = data.rstrip('\x00').split(": ")
+            header_name = header_info[0].lower()
+            header_value = header_info[1]
+
+            if header_name == 'x-varnish':
+                self.log_snapshot['span_id'] = header_value
+            elif header_name == 'x-varnish-trace':
+                self.log_snapshot['trace_id'] = header_value
+            elif header_name == 'x-varnish-parent':
+                self.log_snapshot['parent_id'] = header_value
+            elif header_name == 'x-varnish-debug':
+                self.log_snapshot['debug'] = header_value
+            elif header_name == 'host':
+                ipv4 = header_value
+                port = 0
+                if ":" in header_value:
+                    value = header_value.split(":")
+                    ipv4 = value[0]
+                    port = value[1]
+
+                self.log_snapshot['ipv4'] = ipv4
+                self.log_snapshot['port'] = port
+
+    def __on_snapshot_ready(self):
+        for func in self.callbacks:
+            if func:
+                func(self.log_snapshot.copy())
---
+++ logreader/log/parser.py
@@ -0,0 +1,152 @@
+spans = []
+annotations = []
+
+CLIENT_RECEIVE = 'cr'
+SERVER_RECEIVE = 'sr'
+SERVER_SEND = 'ss'
+
+
+def clear_spans():
+    global spans
+    del spans[:]
+
+
+def clear_annotations():
+    global annotations
+    del annotations[:]
+
+
+def __ts_convert(row_dict, ts_key, convert_func):
+    if ts_key in row_dict:
+        ts_value = row_dict[ts_key]
+        ts = ts_value if ts_value else ''
+        if len(ts):
+            row_dict[ts_key] = convert_func(ts)
+
+
+def __convert_timestamp(timestamp):
+    return int(timestamp.replace('.', ''))
+
+
+def __convert_duration(duration):
+    _duration = duration.replace('.', '').lstrip('0')
+    return int(_duration) if len(_duration) else 0
+
+
+def __convert_ip_to_int(ip):
+    parts = ip.split('.')
+    return (int(parts[0]) << 24) + (int(parts[1]) << 16) + \
+        (int(parts[2]) << 8) + int(parts[3])
+
+
+def replace_client_span_id(client_span_id, backend_span_id):
+    for span_idx in range(len(spans)):
+        span_row = spans[span_idx]
+
+        if span_row['span_id'] == client_span_id:
+            if span_row['trace_id'] == span_row['span_id']:
+                span_row['trace_id'] = backend_span_id
+
+            span_row['span_id'] = backend_span_id
+            spans[span_idx] = span_row
+
+
+def __parse_client_log(row_dict):
+    __ts_convert(row_dict, 'timestamp-abs-Start', __convert_timestamp)
+    __ts_convert(row_dict, 'timestamp-duration-Start', __convert_duration)
+    __ts_convert(row_dict, 'timestamp-duration-Resp', __convert_duration)
+    __ts_convert(row_dict, 'timestamp-abs-Resp', __convert_timestamp)
+
+    if 'trace_id' not in row_dict:
+        row_dict['trace_id'] = row_dict['span_id']
+
+    span = {'span_id': row_dict['span_id'],
+            'parent_id': row_dict['parent_id'],
+            'trace_id': row_dict['trace_id'],
+            'span_name': row_dict['span_name'],
+            'debug': row_dict['debug'],
+            'duration': row_dict['timestamp-duration-Start'],
+            'created_ts': row_dict['timestamp-abs-Start']}
+
+    spans.append(span)
+
+    if row_dict['parent_id'] is not None:
+        child_span = dict()
+        child_span.update(span)
+        child_span['duration'] = row_dict['timestamp-duration-Resp']
+        child_span['created_ts'] = row_dict['timestamp-abs-Resp']
+        spans.append(child_span)
+
+
+def __parse_backend_log(row_dict):
+    __ts_convert(row_dict, 'timestamp-duration-Start', __convert_duration)
+    __ts_convert(row_dict, 'timestamp-abs-Start', __convert_timestamp)
+    __ts_convert(row_dict, 'timestamp-duration-Bereq', __convert_duration)
+    __ts_convert(row_dict, 'timestamp-abs-Bereq', __convert_timestamp)
+    __ts_convert(row_dict, 'timestamp-duration-Beresp', __convert_duration)
+    __ts_convert(row_dict, 'timestamp-abs-Beresp', __convert_timestamp)
+    __ts_convert(row_dict, 'timestamp-duration-BerespBody', __convert_duration)
+    __ts_convert(row_dict, 'timestamp-abs-BerespBody', __convert_timestamp)
+
+    if 'trace_id' not in row_dict:
+        row_dict['trace_id'] = row_dict['span_id']
+
+    if row_dict['ipv4']:
+        row_dict['ipv4'] = __convert_ip_to_int(row_dict['ipv4'])
+
+    begin_tag_data = row_dict['begin'].strip().split(" ")
+    client_span_id = begin_tag_data[1]
+
+    replace_client_span_id(client_span_id, row_dict['span_id'])
+
+    annotation = {
+        'span_id': row_dict['span_id'],
+        'trace_id': row_dict['trace_id'],
+        'span_name': row_dict['span_name'],
+        'service_name': row_dict['span_name'],
+        'value': 'cs',
+        'ipv4': row_dict['ipv4'],
+        'port': row_dict['port'],
+        'a_timestamp': row_dict['timestamp-abs-Start'],
+        'duration': row_dict['timestamp-duration-Start']
+    }
+
+    client_start_annotation = dict()
+    client_start_annotation.update(annotation)
+    client_start_annotation['a_timestamp'] = row_dict['timestamp-abs-Bereq']
+    client_start_annotation['duration'] = row_dict['timestamp-duration-Bereq']
+    client_start_annotation['value'] = SERVER_RECEIVE
+
+    server_rec_annotation = dict()
+    server_rec_annotation.update(annotation)
+    server_rec_annotation['a_timestamp'] = row_dict['timestamp-abs-Beresp']
+    server_rec_annotation['duration'] = row_dict['timestamp-duration-Beresp']
+    server_rec_annotation['value'] = SERVER_SEND
+
+    server_resp_annotation = dict()
+    server_resp_annotation.update(annotation)
+    server_resp_annotation['a_timestamp'] = row_dict[
+        'timestamp-abs-BerespBody']
+    server_resp_annotation['duration'] = row_dict[
+        'timestamp-duration-BerespBody']
+    server_resp_annotation['value'] = CLIENT_RECEIVE
+
+    annotations.append(annotation)
+    annotations.append(client_start_annotation)
+    annotations.append(server_rec_annotation)
+    annotations.append(server_resp_annotation)
+
+
+def parse_log_row(row_dict):
+    if 'debug' not in row_dict:
+        row_dict['debug'] = 0
+
+    if 'parent_id' not in row_dict:
+        row_dict['parent_id'] = 0
+
+    req_type = row_dict['request_type']
+
+    if req_type == 'c':
+        __parse_client_log(row_dict)
+    elif req_type == 'b':
+        __parse_backend_log(row_dict)
