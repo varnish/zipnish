1a01fccc510dc6d3fe0dee8bcc03d8ffca741b9f
Marius Magureanu <mariusmagureanu@live.dk>
2016-04-27 19:36:30 +0000

    License classifier got updated.

--- setup.py
+++ setup.py
@@ -19,8 +19,7 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'Topic:: Internet:: Log Analysis',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: Apache Software License',     
         'Programming Language :: Python :: 2.7',
     ],
     author='Adeel Shahid, Per Buer, Marius Magureanu',
