98ce3150b6f01e9513fa9ce2da1d505e6350aea6
Marius Magureanu <mariusmagureanu@live.dk>
2016-05-09 15:51:50 +0200

    Read long description from READMe.rst

--- setup.py
+++ setup.py
@@ -1,21 +1,26 @@
 import sys
-from setuptools import setup
 
+from setuptools import setup, find_packages
 
 if sys.version_info < (2, 7):
     sys.exit('Zipnish requires at least Python 2.7, please upgrade and try again.')
 
-setup_requires = []
+
+def read(filename):
+    with open(filename) as f:
+        return f.read()
+
 
 install_requires = [
-    'simplemysql',
+    'simplemysql', 'flask', 'sqlalchemy',
 ]
 
 setup(
+    include_package_data=True,
     name='zipnish',
-    version='0.1.0',
+    version='0.1.2',
     description='zipnish',
-    long_description='Micro-services monitoring tool based on Varnish Cache.',
+    long_description=read('README.rst'),
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
@@ -26,13 +31,15 @@ setup(
     author_email='marius@varnish-software.com',
     url='https://github.com/varnish/zipnish.git',
     license='Apache License 2.0',
-    packages=['logreader', 'logreader.log'],
+    platforms='Linux',
+    packages=find_packages(exclude=['logreader.test']),
     zip_safe=False,
     install_requires=install_requires,
-    setup_requires=setup_requires,
     package_data={
-        'logreader': ['default.cfg']
+        'logreader': ['default.cfg'],
     },
-    entry_points={'console_scripts': ['logreader = logreader.app:main']},
+    entry_points={'console_scripts': ['zipnish-logreader = logreader.app:main',
+                                      'zipnish-ui = ui.flask_app:main'],
+                  },
     scripts=[]
 )
